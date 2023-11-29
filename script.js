9de8200ee630c644066726069253b99528d8e33a
Mila Makarenko <mila.makarenko@tptlive.ee>
2023-11-29 13:55:43 +0200

    Add nunjucks

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 
- const express = require('express')
-const app = express()
+ const express = require('express');
+const app = express();
+const nunjucks = require('nunjucks');
 
 app.get('/', function (req, res) {
-  res.send('Hello VM22!')
-})
+  let name = "Mila Makarenko";
+  res.sendFile('index.njk', {name})
+});
+
+app.get('/', function (req, res) {
+  res.render ('about.njk')
+});
 
 app.listen(3000);
+console.log('Server started on http//localhost:3000');
