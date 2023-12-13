d3641e78e2009bb9d098a2d67bdbcb59c45e56d6
Mila Makarenko <mila.makarenko@tptlive.ee>
2023-12-13 14:15:05 +0200

    Nunjuks

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
- const express = require('express');
+const express = require('express');
 const app = express();
 const nunjucks = require('nunjucks');
 
@@ -9,13 +8,11 @@ app.use(express.urlencoded())
 app.get('/', function (req, res) {
   let name = req.query.name;
   let age = req.query.age;
-  res.re('index.njk', {name, age})
+  res.re('answer.njk', {name, age})
 });
 
-app.post('/answer', function (req, res) {
-  let name = req.query.name;
-  let age = req.query.age;
-  res.re('answer.njk', {name, age})
+app.post('/about', function (req, res) {
+  res.sendFile(__dirname + '/indexe.html')
 });
 
 app.listen(3000);
